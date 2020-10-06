a56a8416913029818740b726a509d578ce4caa0a
Reece McTiernan <rmctiernan@pm.me>
2020-10-06 15:27:49 +0100

    Add devise trackable to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   def send_on_create_confirmation_instructions
     # Intentionally do nothing here!
