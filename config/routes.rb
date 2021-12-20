6f7df62ae72b45f5efc3f8865e42cc22b7af65f6
Reece <rmctiernan@pm.me>
2021-12-20 09:39:28 +0000

    Fix error on user redirect after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'entries#index', as: 'user_root'
   root to: "entries#index"
 end
