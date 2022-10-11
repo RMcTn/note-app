abc11b8444ea2237550e9a365840e40420c74da5
Reece McTiernan <rmctiernan@pm.me>
2022-10-11 21:15:34 +0100

    Actual turbo stream working now

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,5 +2,7 @@ class Entry < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_rich_text :content
-  after_create_commit -> { broadcast_prepend_to user, target: "entry-container" }
+  after_create_commit -> { broadcast_prepend_to user, target: "entries-container" }
+  after_destroy_commit -> { broadcast_remove_to user }
+  after_update_commit -> { broadcast_update_to user }
 end
